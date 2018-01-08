3e898e8cce43813a8c0eafcc6c6376ce1e6eafac
lians <lian.setter@netapp.com>
2018-01-08 13:31:45 +0200

    music to the face yo

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,5 +1,5 @@
 export class MainController {
-  constructor ($timeout, webDevTec, toastr) {
+  constructor($timeout, webDevTec, toastr, $scope) {
     'ngInject';
 
     this.awesomeThings = [];
@@ -28,5 +28,46 @@ export class MainController {
   showToastr() {
     this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
     this.classAnimation = '';
+  }
+
+  /*
+   -------------------------------------------------
+   */
+  videoPlayer() {
+
+// Grab elements, create settings, etc.
+    var video = document.getElementById('video');
+
+// Get access to the camera!
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+      });
+    }
+
+// Elements for taking the snapshot
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var video = document.getElementById('video');
+
+// Trigger photo take
+    document.getElementById("snap").addEventListener("click", function() {
+      context.drawImage(video, 0, 0, 640, 480);
+
+      // Get an image dataURL from the canvas.
+      var imageDataURL = canvas.toDataURL('image/png');
+      console.log("################# imageDataURL="+imageDataURL)
+
+      // Set the dataURL as source of an image element, showing the captured photo.
+      //image.setAttribute('src', imageDataURL);
+
+      // Set the href attribute of the download button.
+      document.querySelector('#dl-btn').href = imageDataURL;
+    });
+
   }
 }
